7e9daed8db4a826f4ad49610c69ac75b92868031
likes983710494 <983710494@qq.com>
2023-12-26 11:57:14 +0800

    装饰设计

--- Assets/Scripts/GLTFLoad/LoadGltf.cs
+++ Assets/Scripts/GLTFLoad/LoadGltf.cs
@@ -11,14 +11,31 @@ namespace PlanesWalker
 {
     public class LoadGltf : MonoBehaviour
     {
-        private void Start()
+		public Camera BackRoundCamera;
+		public Transform ZeroParent;
+
+		private void Start()
         {
 
             GLTFUtilityScript.Instance.ImportGLTFAsync(Application.streamingAssetsPath + "/NewProject.gltf", asd);
         }
         public void asd(GameObject result, AnimationClip[] clip)
         {
-          
-        }
+			result.transform.position = ZeroParent.position;
+			result.transform.localScale=Vector3.one;
+			result.transform.SetParent(ZeroParent);
+			foreach (Transform child in result.transform)
+			{
+				if(child.GetComponent<Camera>()!=null)
+				{
+					Debug.Log(child.name);
+					child.GetComponent<Camera>().enabled = false;
+				}
+			}		
+			//创建一个新的相机
+				//BackRoundCamera.gameObject.SetActive(true);
+			
+
+		}
     }
 }
---
+++ Assets/Scripts/Management/DecorativeDesignRight.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Menu;
+/// <summary>
+/// 左侧脚本
+/// </summary>
+public class DecorativeDesignRight : MonoBehaviour
+{
+	public Dropdown DropdownBranch;//分部
+	public Dropdown DropdownSubentry;//分项
+	void Start()
+	{
+		DropdownBranch.onValueChanged.AddListener(BranchDropdownChange);
+		DropdownSubentry.onValueChanged.AddListener(SubentryDropdownChange);
+
+		DropdownSubentry.interactable = false;
+	}
+
+
+	void Update()
+	{
+
+	}
+	/// <summary>
+	/// 分部选项
+	/// </summary>
+	/// <param name="index">下拉框下标</param>
+	public void BranchDropdownChange(int index)
+	{
+		//清空数组
+		DropdownSubentry.ClearOptions();
+		DropdownSubentry.interactable = true;
+		switch (index)
+		{
+			case 0:
+				DropdownSubentry.AddOptions(new List<string> { "找平层", "整体面层", "块料面层", "其他面层", "其他项目" });
+				break;
+			case 1:
+				DropdownSubentry.AddOptions(new List<string> { "墙、柱面抹灰", "镶贴块料面层", "墙、装饰面", "隔断、幕墙", "墙、柱面吸音" });
+				break;
+			case 2:
+				DropdownSubentry.AddOptions(new List<string> { "天棚抹灰", "天棚龙骨", "天棚饰面", "雨篷" });
+				break;
+			case 3:
+				DropdownSubentry.AddOptions(new List<string> { "木材面油漆", "金属面油漆", "抹灰面油漆、涂料", "基层处理", "裱糊" });
+				break;
+			case 4:
+				DropdownSubentry.AddOptions(new List<string> { "柜类、货架", "装饰线条", "扶手、栏杆、栏板", "暖气罩", "浴厕配件", "招牌、灯箱", "美术字", "零星木装饰", "工艺门扇" });
+				break;
+
+		}
+
+	}
+	/// <summary>
+	/// 分项下拉选项
+	/// </summary>
+	/// <param name="index"></param>
+	public void SubentryDropdownChange(int index)
+	{
+		//设置关联模型
+		
+	}
+}
