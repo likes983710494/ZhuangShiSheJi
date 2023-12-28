6ec50f95a65485ccfaf22160d8f50c18589ddc37
likes983710494 <983710494@qq.com>
2023-12-28 11:13:59 +0800

    限额分解新增分部也可输入 修改计算金额逻辑

--- Assets/Scripts/Management/AntidiastoleManager.cs
+++ Assets/Scripts/Management/AntidiastoleManager.cs
@@ -21,55 +21,65 @@ public class AntidiastoleManager : MonoBehaviour
 	public List<InputField> 天棚工程InputField = new List<InputField>();//天棚工程
 	public List<InputField> 油漆涂料InputField = new List<InputField>();//油漆涂料及裱糊工程
 	public List<InputField> 其他装饰InputField = new List<InputField>();//其他装饰工程
-	
 
-	public Text 楼地面装饰_Amount;
-	public Text 墙柱面装饰_Amount;
-	public Text 天棚工程_Amount;
-	public Text 油漆涂料_Amount;
-	public Text 其他装饰_Amount;
+
+	public List<InputField> Amount_InputField = new List<InputField>();//第二列 分部 
+
+
+
 
 	public Text 总金额_Amount;//由老师设定，后台发送
 
 	private float 总金额_Cache=0;//和总金额比较
 	private float proportion = 0;//cache占总金额比例
-	private bool is楼地面装饰, is墙柱面装饰, is天棚工程, is油漆涂料, is其他装饰;
+	private bool is楼地面装饰, is墙柱面装饰, is天棚工程, is油漆涂料, is其他装饰, isAmount;
 	void Start()
     {
 		affirmButton.gameObject.SetActive(false);
+		affirmButton.onClick.AddListener(PromptContentAffirm);
 		submitButton.onClick.AddListener(SubmitEachAmount);
+
+		for (int i = 0; i < Amount_InputField.Count; i++)
+		{
+			Amount_InputField[i].onValueChanged.AddListener((string value) => {
+				
+				isAmount = SetIsSubmit(Amount_InputField, is楼地面装饰);
+			});
+		}
+
 		for (int i = 0; i < 楼地面装饰InputField.Count; i++)
 		{
 			楼地面装饰InputField[i].onValueChanged.AddListener((string value) => {
-				楼地面装饰_Amount.text = EachAmount(楼地面装饰InputField).ToString();
-				is楼地面装饰= SetIsSubmit(楼地面装饰InputField, is楼地面装饰);
+				
+				is楼地面装饰 = SetIsSubmit(楼地面装饰InputField, is楼地面装饰);
+
 			});
 		}
 		for (int i = 0; i < 墙柱面装饰InputField.Count; i++)
 		{
 			墙柱面装饰InputField[i].onValueChanged.AddListener((string value) => {
-				墙柱面装饰_Amount.text = EachAmount(墙柱面装饰InputField).ToString();
+				
 				is墙柱面装饰 = SetIsSubmit(墙柱面装饰InputField, is墙柱面装饰);
 			});
 		}
 		for (int i = 0; i < 天棚工程InputField.Count; i++)
 		{
 			天棚工程InputField[i].onValueChanged.AddListener((string value) => {
-				天棚工程_Amount.text = EachAmount(天棚工程InputField).ToString();
+				
 				is天棚工程 = SetIsSubmit(天棚工程InputField, is天棚工程);
 			});
 		}
 		for (int i = 0; i < 油漆涂料InputField.Count; i++)
 		{
 			油漆涂料InputField[i].onValueChanged.AddListener((string value) => {
-				油漆涂料_Amount.text = EachAmount(油漆涂料InputField).ToString();
+				
 				is油漆涂料 = SetIsSubmit(油漆涂料InputField, is油漆涂料);
 			});
 		}
 		for (int i = 0; i < 其他装饰InputField.Count; i++)
 		{
 			其他装饰InputField[i].onValueChanged.AddListener((string value) => {
-				其他装饰_Amount.text = EachAmount(其他装饰InputField).ToString();
+				
 				is其他装饰 = SetIsSubmit(其他装饰InputField, is其他装饰);
 			});
 		}
@@ -79,7 +89,7 @@ public class AntidiastoleManager : MonoBehaviour
     void Update()
     {
 		//
-		if (is楼地面装饰 == true && is墙柱面装饰 == true && is天棚工程 == true && is油漆涂料 == true && is其他装饰 == true)
+		if (is楼地面装饰 == true && is墙柱面装饰 == true && is天棚工程 == true && is油漆涂料 == true && is其他装饰 == true&& isAmount==true)
 		{
 			submitButton.interactable = true;
 		}
@@ -148,7 +158,7 @@ public class AntidiastoleManager : MonoBehaviour
 	public void SubmitEachAmount()
 	{
 
-		总金额_Cache = float.Parse(楼地面装饰_Amount.text) + float.Parse(墙柱面装饰_Amount.text) + float.Parse(天棚工程_Amount.text) + float.Parse(油漆涂料_Amount.text) + float.Parse(其他装饰_Amount.text);
+		总金额_Cache = float.Parse(Amount_InputField[0].text) + float.Parse(Amount_InputField[1].text) + float.Parse(Amount_InputField[2].text) + float.Parse(Amount_InputField[3].text) + float.Parse(Amount_InputField[4].text);
 		proportion = 总金额_Cache / float.Parse(总金额_Amount.text);
 		promptContentPlane.SetActive(true);
 		if (proportion < 0.6)//小于百分之60
@@ -156,16 +166,50 @@ public class AntidiastoleManager : MonoBehaviour
 			Debug.Log("分部工程的汇总金额太低，请重新调整");
 			promptContentText.text = "分部工程的汇总金额太低，请重新调整";
 		}
-		if (proportion > 0.6 && proportion < 1)
+		if (proportion > 0.6 && proportion <= 1)
 		{
-			Debug.Log("分部工程的汇总金额符合");
-			promptContentText.text = "分部工程的汇总金额符合";
-			affirmButton.gameObject.SetActive(true);
-			Unit.UnitDollarData.楼地面装饰_Amount = 楼地面装饰_Amount.text;
-			Unit.UnitDollarData.墙柱面装饰_Amount = 墙柱面装饰_Amount.text;
-			Unit.UnitDollarData.天棚工程_Amount = 天棚工程_Amount.text;
-			Unit.UnitDollarData.油漆涂料_Amount = 油漆涂料_Amount.text;
-			Unit.UnitDollarData.其他装饰_Amount = 其他装饰_Amount.text;
+			
+
+			//分部金额和分项金额要相等 否则提示
+
+
+			if (float.Parse(Amount_InputField[0].text) != EachAmount(楼地面装饰InputField))
+			{
+				Debug.Log("金额错误，请调整楼地面装饰的分部和分项的金额。");
+				promptContentText.text = "金额错误，请调整楼地面装饰的分部和分项的金额。";
+				affirmButton.gameObject.SetActive(false);
+			}
+			 else if (float.Parse(Amount_InputField[1].text) != EachAmount(墙柱面装饰InputField))
+			{
+				Debug.Log("金额错误，请调整楼地面装饰的分部和分项的金额。");
+				promptContentText.text = "金额错误，请调整楼墙、柱面装饰与隔断、幕墙工程的分部和分项的金额。";
+				affirmButton.gameObject.SetActive(false);
+			}
+			else if(float.Parse(Amount_InputField[2].text) != EachAmount(天棚工程InputField))
+			{
+				Debug.Log("金额错误，请调整楼地面装饰的分部和分项的金额。");
+				promptContentText.text = "金额错误，请调整楼天棚工程的分部和分项的金额。";
+				affirmButton.gameObject.SetActive(false);
+			}
+			else if (float.Parse(Amount_InputField[3].text) != EachAmount(油漆涂料InputField))
+			{
+				Debug.Log("金额错误，请调整楼地面装饰的分部和分项的金额。");
+				promptContentText.text = "金额错误，请调整油漆、涂料及裱糊工程的分部和分项的金额。";
+				affirmButton.gameObject.SetActive(false);
+			}
+			else if (float.Parse(Amount_InputField[4].text) != EachAmount(其他装饰InputField))
+			{
+				Debug.Log("金额错误，请调整楼地面装饰的分部和分项的金额。");
+				promptContentText.text = "金额错误，请调其他装饰的分部和分项的金额。";
+				affirmButton.gameObject.SetActive(false);
+			}
+			else
+			{
+				Debug.Log("工程金额符合,请点击确认进行下一项");
+				promptContentText.text = "工程金额符合,请点击确认进行下一项。";
+				affirmButton.gameObject.SetActive(true);
+				
+			}
 
 		}
 		if (proportion > 1)
@@ -173,6 +217,7 @@ public class AntidiastoleManager : MonoBehaviour
 			Debug.Log("分部工程的汇总金额太高，请重新调整");
 			promptContentText.text = "分部工程的汇总金额太高，请重新调整";
 		}
+		
 	}
 
 	/// <summary>
@@ -180,10 +225,15 @@ public class AntidiastoleManager : MonoBehaviour
 	/// </summary>
 	public void PromptContentAffirm()
 	{
-		//隐藏模块
-		//将分部限额金额通知到装饰设计
-		//提交确定信息
+		//隐藏模块-已非代码实现
 
+		//提交确定信息
+		//金额数据储存	，将分部限额金额通知到装饰设计
+		Unit.UnitDollarData.楼地面装饰_Amount = Amount_InputField[0].text;
+		Unit.UnitDollarData.墙柱面装饰_Amount = Amount_InputField[1].text;
+		Unit.UnitDollarData.天棚工程_Amount = Amount_InputField[2].text;
+		Unit.UnitDollarData.油漆涂料_Amount = Amount_InputField[3].text;
+		Unit.UnitDollarData.其他装饰_Amount = Amount_InputField[4].text;
 	}
 
 }
