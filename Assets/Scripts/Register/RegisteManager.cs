ccd9a1d8ccbba0c60450a779a3ae44d48bf7545c
likes983710494 <983710494@qq.com>
2024-04-03 14:57:19 +0800

    登录界面

---
+++ Assets/Scripts/Register/RegisteManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegisteManager : MonoBehaviour
+{
+
+    void Awake()
+    {
+
+    }
+    void Start()
+    {
+
+    }
+    public void RegisteQuit()
+    {
+        Application.Quit();
+    }
+
+
+}
